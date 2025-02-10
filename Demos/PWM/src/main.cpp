ca3f3974ef2add0cfd73f28c44bcaff95a91f33a
Robbie Leslie <leslier@lafayette.edu>
2025-02-10 14:53:09 -0500

    Started work on detecting fan speed

--- Demos/PWM/src/main.cpp
+++ Demos/PWM/src/main.cpp
@@ -1,10 +1,10 @@
 #include "Arduino.h"
 #include "PWMFunctions.h"
 
-#define PWM_PIN0 17  // Choose a valid PWM-capable GPIO pin
+#define PWM_PIN0 16  // Choose a valid PWM-capable GPIO pin
 
 void setup() {
-    setPWM(PWM_PIN0, 25000, 10);  // 1 kHz, 50% duty cycle
+    setPWM(PWM_PIN0, 25000, 20);  // 1 kHz, 50% duty cycle
 }
 
 void loop() {
